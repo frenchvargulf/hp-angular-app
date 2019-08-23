815bebb96f3552b09cf8880a9bd3dd9bd0923a79
MartaAnastazja <mcm.czarnecka@gmail.com>
2019-08-23 15:53:17 +0200

    Update NgModule

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from 'src/app/material-module';
+import { CardsComponent } from './cards/cards.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
